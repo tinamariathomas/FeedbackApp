302d7c56fd12c941665aee96391fa61d47d91991
tinamariathomas <codertina@gmail.com>
2016-01-12 12:50:37 +0530

    Adding integration-test for valid event-creation

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -5,6 +5,7 @@ class EventsTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'events/index'
   end
+
   test "invalid event creation" do
     get new_event_path
     before_count = Event.count
@@ -12,5 +13,16 @@ class EventsTest < ActionDispatch::IntegrationTest
     after_count = Event.count
     assert_equal before_count,after_count
     assert_template 'events/new'
+  end
+
+  test "valid event creation" do
+    get new_event_path
+    before_count = Event.count
+    post events_path, event: {"name"=>"DisTech2014", "date(1i)"=>"2016", "date(2i)"=>"1", "date(3i)"=>"12"};
+    after_count = Event.count
+    assert_equal before_count+1,after_count
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_template 'events/index'
   end
 end
