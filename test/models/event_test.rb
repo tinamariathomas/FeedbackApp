1aa51a82d662bb04c767f91a6673b9c2ad89ac4f
tinamariathomas <codertina@gmail.com>
2016-01-12 12:02:06 +0530

    Adding tests for validity of event model

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @valid_event = Event.new(name: "vodQA", date:"2016-01-22")
+  end
+
+  test "Event validity" do
+    assert @valid_event.valid?
+  end
+
+  test "Event should be invalid if event name is empty" do
+    @valid_event.name=""
+    assert_not @valid_event.valid?
+  end
+
 end
