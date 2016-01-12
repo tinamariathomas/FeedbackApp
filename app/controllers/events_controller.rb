98bdc2b70d76c5691b86704ff0fecee346968f19
tinamariathomas <codertina@gmail.com>
2016-01-12 12:45:07 +0530

    Adding test case for invalid event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,17 +6,21 @@ class EventsController < ApplicationController
   def create
     event = params[:event]
     date = Date.new event["date(1i)"].to_i, event["date(2i)"].to_i, event["date(3i)"].to_i
-    @event = Event.new(name:event[:name],date:date)
-    @event.save
-    redirect_to root_url
+    @event = Event.new(name: event[:name], date: date)
+    # debugger
+    if @event.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
-  def index
-    @events = Event.all
+    def index
+      @events = Event.all
+    end
+
+    def show
+
+    end
+
   end
-
-  def show
-
-  end
-
-end
--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -5,4 +5,12 @@ class EventsTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'events/index'
   end
+  test "invalid event creation" do
+    get new_event_path
+    before_count = Event.count
+    post events_path, event: {"name"=>"", "date(1i)"=>"2016", "date(2i)"=>"1", "date(3i)"=>"12"};
+    after_count = Event.count
+    assert_equal before_count,after_count
+    assert_template 'events/new'
+  end
 end
