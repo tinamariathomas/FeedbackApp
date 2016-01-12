54ce28dde04df74248f72235bc951152ab2ace33
tinamariathomas <codertina@gmail.com>
2016-01-12 15:48:50 +0530

    Tests for Talks model

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,5 +4,7 @@ class Talk < ActiveRecord::Base
   # end
   belongs_to :event
   validates :event_id, presence: true
+  validates :title, presence: true
+  validates :presenters, presence: true
 
 end
--- test/models/talk_test.rb
+++ test/models/talk_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TalkTest < ActiveSupport::TestCase
   def setup
     @event = events(:vodqa)
-    @talk = Talk.new(title: "Agile Testing",presenters: "C3PO",event_id:@event.id);
+    @talk = Talk.new(title: "Agile Testing", presenters: "C3PO", event_id: @event.id);
   end
 
   test "Talk should be valid if it associated with a proper Event" do
@@ -12,6 +12,16 @@ class TalkTest < ActiveSupport::TestCase
 
   test "Talk should be invalid if it is not associated with a proper event" do
     @talk.event_id =nil
+    assert_not @talk.valid?
+  end
+
+  test "Talk title should not be blank" do
+    @talk.title=nil
+    assert_not @talk.valid?
+  end
+
+  test "Talk presenters should not be blank" do
+    @talk.presenters=nil
     assert_not @talk.valid?
   end
 end
