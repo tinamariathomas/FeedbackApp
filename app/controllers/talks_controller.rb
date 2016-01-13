a464a970ad1085cf8c9e57432a56f3c15cab2c83
tinamariathomas <codertina@gmail.com>
2016-01-13 10:27:18 +0530

    Show all talks and create talks for an event

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,5 +1,23 @@
 class TalksController < ApplicationController
   def new
-    @talk = Talk.new
+    @event = Event.find(params[:event_id])
+    @talk = @event.talks.new
+  end
+
+  def index
+    @event = Event.find(params[:event_id])
+    @talks = @event.talks
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @talk = @event.talks.build(talk_params)
+    @talk.save
+    redirect_to event_talks_path
+
+  end
+  private
+  def talk_params
+    params[:talk].permit(:title,:presenters)
   end
 end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,7 +1,4 @@
 class Talk < ActiveRecord::Base
-  # def setup
-  #   # @valid = Event.new(name: "vodQA", date:"2016-01-22")
-  # end
   belongs_to :event
   validates :event_id, presence: true
   validates :title, presence: true
