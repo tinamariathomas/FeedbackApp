77be71f37869a014c8312fd2b4a29a3a288b3a01
tinamariathomas <codertina@gmail.com>
2016-01-14 11:05:52 +0530

    Model + migration for admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113072753) do
+ActiveRecord::Schema.define(version: 20160114052951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
